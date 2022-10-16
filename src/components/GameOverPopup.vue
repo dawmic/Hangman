f91241f10a38bc10ecc7a980e14d3f53cf281a14
Dawid <dawmic19@gmail.com>
2022-10-16 20:53:55 +0200

    Added image and moved animation styles to other file

--- src/components/GameOverPopup.vue
+++ src/components/GameOverPopup.vue
@@ -1,96 +1,59 @@
 <script setup>
-
+import NewGameButton from './NewGameButton.vue';
+const props = defineProps(['correct_word']);
 </script>
 <template>
-<div class="popup-container">
-  <img width="128" height="128" src="../assets/hangman_go.png" alt="game over hangman" class="popup-image">
-  <!--<a href="https://www.flaticon.com/free-icons/hangman" title="hangman icons">Hangman icons created by Smashicons - Flaticon</a> -->
-  <p class="popup-paragraph">GAME OVER</p>
-  <button class="popup-button">NEW WORD</button>
-</div>
+  <div class="popup-container">
+    <img
+     
+      height="200"
+      src="../assets/cropped_hangman_end.png"
+      alt="game over hangman"
+      class="popup-image"
+    />
+    <!--<a href="https://www.flaticon.com/free-icons/hangman" title="hangman icons">Hangman icons created by Smashicons - Flaticon</a> -->
+    <h1 class="popup-header">GAME OVER</h1>
+    <p class="popup-paragraph">The correct password is: <span class="popup-paragraph__key">{{props.correct_word}}</span></p>
+    <NewGameButton>
+     TRY AGAIN
+      </NewGameButton>
+  </div>
 </template>
-<style lang='scss' scoped>
-  .popup-container{
-    margin: 0;
-    padding: 0;
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100vh;
-    width: 100vw;
-    filter: blur(1px);
-    background: #191919;
-   opacity: .99;
-    z-index: 3;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-around;
+<style lang="scss">
+// container styles are located in main.css
+  .popup-image {
+    -webkit-animation: bounce-in-top 1.1s both;
+	        animation: bounce-in-top 1.1s both;
+  }
+  .popup-header {
+    font-size: 4rem;
+    font-weight: 700;
+    letter-spacing: 3px;
+    color: #ffffff;
+   // animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
+   
+	-webkit-animation: rotate-in-center 0.6s cubic-bezier(0.250, 0.460, 0.450, 0.940) 1s both;
+	        animation: rotate-in-center 0.6s cubic-bezier(0.250, 0.460, 0.450, 0.940) 1s both;
 
-    .popup-image{
-      -webkit-animation: slide-in-top 1s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
-	        animation: slide-in-top 1s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
-    }
-    .popup-paragraph{
-      font-size: 4rem;
-      font-weight: 700;
-      letter-spacing: 3px;
-      color: #FFFFFF;
-      animation: shake .82s cubic-bezier(.36,.07,.19,.97) both;
-      
-    }
-    .popup-button{
-      outline: none;
-      border: 2px #FEB800 dashed;
-      border-radius: 2rem;
-      background: transparent;
-      color: #FEB800;
-      width: 22rem;
-      height: 5rem;
-      font-size: 2rem;
-    }
+  }
+  .popup-paragraph{
+    margin: 0 1rem;
+    text-align: center;
+font-size: 2rem;
+color: #ececec;
+font-weight: 500;
+.popup-paragraph__key{
+  font-size: 2.5rem;
+  color: #ffc93c;
+  font-weight: 600;
+  letter-spacing: 1px;
+}
+
   }
   
-  @keyframes shake {
-  10%, 90% {
-    transform: translate3d(-1px, 0, 0);
-  }
   
-  20%, 80% {
-    transform: translate3d(2px, 0, 0);
-  }
 
-  30%, 50%, 70% {
-    transform: translate3d(-4px, 0, 0);
-  }
 
-  40%, 60% {
-    transform: translate3d(4px, 0, 0);
-  }
-}
-@-webkit-keyframes slide-in-top {
-  0% {
-    -webkit-transform: translateY(-1000px);
-            transform: translateY(-1000px);
-    opacity: 0;
-  }
-  100% {
-    -webkit-transform: translateY(0);
-            transform: translateY(0);
-    opacity: 1;
-  }
-}
-@keyframes slide-in-top {
-  0% {
-    -webkit-transform: translateY(-1000px);
-            transform: translateY(-1000px);
-    opacity: 0;
-  }
-  100% {
-    -webkit-transform: translateY(0);
-            transform: translateY(0);
-    opacity: 1;
-  }
-}
+
 
 </style>
